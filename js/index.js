0ea449954caec956db9c544742665b1bf74f7324
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-19 00:28:04 -0600

    finished dice game

--- js/index.js
+++ js/index.js
@@ -1,20 +1,18 @@
 "use strict"
 
-let randomNumber1 = Math.floor(Math.random() * 7) + 1;
-// console.log(randomNumber1);
-let randomNumber2 = Math.floor(Math.random() * 7) + 1;
+// random numbers for dice roll
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-$(window).on('load',function () {
-	$('.img1').attr('src', `images/dice${randomNumber1}.png`);
-	$('.img2').attr('src', `images/dice${randomNumber2}.png`);
-	console.log(randomNumber1)
-	console.log(randomNumber2)
-})
+// setting images to number rolled
+$('.img1').attr('src', `images/dice${randomNumber1}.png`);
+$('.img2').attr('src', `images/dice${randomNumber2}.png`);
 
+// if statement to change h1 element
 if (randomNumber1 > randomNumber2) {
 	$('h1').html('Player 1 Wins!')
 } else if(randomNumber1 === randomNumber2) {
-	$('h1').html('Roll Again')
+	$('h1').html('Tie! Roll Again')
 } else {
 	$('h1').html('Player 2 Wins!')
 }
